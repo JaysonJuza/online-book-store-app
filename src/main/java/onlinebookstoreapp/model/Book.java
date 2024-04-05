bf0bead365480a0a46e908ac23f2ccb0c9add9f3
Yevhen Nedashkivskyi <kesh040593@ukr.net>
2024-04-05 17:07:26 +0300

    added mvnw and mvnw.cmd into .gitignore file. Change annotation @Data to @Setter, @Getter. Added properties for variable param: unique

--- src/main/java/onlinebookstoreapp/model/Book.java
+++ src/main/java/onlinebookstoreapp/model/Book.java
@@ -6,10 +6,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.math.BigDecimal;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity(name = "books")
-@Data
+@Getter
+@Setter
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,7 +20,7 @@ public class Book {
     private String title;
     @Column(nullable = false)
     private String author;
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private BigDecimal isbn;
     @Column(nullable = false)
     private BigDecimal price;
